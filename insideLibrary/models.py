df22a1cb829a73eff330ced77f589525e778dc00
Shivani Agarwal <shivaniagar7@gmail.com>
2024-11-26 19:00:23 +0530

    Initial Commit

---
+++ insideLibrary/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from datetime import date
+# Create your models here.
+
+class Book(models.Model):
+    title= models.CharField(max_length=200)
+    authors= models.TextField(max_length=35)
+    isbn= models.CharField(max_length=20, unique=True)
+    publisher= models.TextField(max_length=50)
+    num_pages= models.IntegerField()
+    stock= models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+    
+class Member(models.Model):
+    name= models.CharField(max_length=100)
+    email= models.EmailField(max_length=100, unique=True)
+    existing_debt= models.IntegerField( default=0)
+
+    def __str__(self):
+        return self.name
+    
+class Transaction(models.Model):
+    book= models.ForeignKey(Book, on_delete=models.CASCADE ) #it will delete the transaction related to book instance when book will be deleted. 
+    member= models.ForeignKey(Member, on_delete=models.CASCADE)
+    issue_date= models.DateField(auto_now_add=True) # it will set the field to the current date when the object is created.
+    return_date= models.DateField(null=True, blank=True) # initially date will be null and field will be blank and no validation error will occur. 
+    fee= models.IntegerField()
+
+    def is_overdue(self):
+        if self.return_date: #checks if return_date exists or not
+            return date.today()> self.return_date #if current date is greater than return date, then transaction is overdue
+        return False #otherwise it will return false
+
+
---
+++ insideLibrary/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from .models import Book, Member, Transaction
+from django.http import JsonResponse
+import requests
+
+
+# Create your views here.
+
+# to view all books
+def book_list(request): #request is HTTP request made by user (GET or POST)
+    books= Book.objects.all() #selecting all the books to render in a table
+    return render(request, 'books/book_list.html', {'books': books}) # in book_list.html template we will write how we want to display the fetched books into the UI.
+
+
+# to add a new book
+def add_book(request):
+    if request.method== 'POST':
+        title= request.POST['title']
+        authors= request.POST['authors']
+        isbn= request.POST['isbn']
+        publisher= request.POST['publisher']
+        num_pages= request.POST['num_pages']
+        stock= request.POST['stock']
+        Book.objects.create(title= title, authors=authors,isbn=isbn, publisher=publisher, num_pages=num_pages, stock=stock ) #we are creating an object with these fields
+        return redirect('book_list')
+    return render(request, 'books/add_book.html')
+
+# to import books from API
+def import_books(request):
+    if request.method== 'POST':
+        title= request.POST.get('title', '')
+        page= request.POST.get('page', 1)
+        response= requests.get(f'https://frappe.io/api/method/frappe-library?page={page}&title={title}')
+        books= response.json().get('message', [])
+        print(books)
+
+        for book in books:
+            Book.objects.get_or_create(
+                title= book['title'],
+                authors= book['authors'],
+                isbn= book['isbn'],
+                publisher= book['publisher'],
+                num_pages= int(book['num_pages']),
+            )
+
+        return JsonResponse({'status': 'Books imported!'})
+    return render(request, 'books/import_books.html')
+
+
